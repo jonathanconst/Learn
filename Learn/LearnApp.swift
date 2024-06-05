ca710e89b55b0702ffc0f94fcb58d01b062b9195
Jonathan Constantinos <jona.jc01@gmail.com>
2024-06-05 12:48:00 +0200

    Initial Commit

---
+++ Learn/LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnApp.swift
+//  Learn
+//
+//  Created by Jonathan Constantinos on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
